724adad9b466d99979a7f5690c58da141c41f583
lucasleone03 <lucasagustinleone2003@gmail.com>
2023-10-31 16:29:17 -0300

    created project

---
+++ chatbot_api/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from chatbot_api.users.models import User
+from chatbot_api.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
